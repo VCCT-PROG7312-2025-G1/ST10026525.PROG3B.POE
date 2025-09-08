ae4d37404afa99063f857f27fab05845675e5b2e
nyanem <ST10026525@vcconnect.edu.za>
2025-09-08 21:37:09 +0200

    Report form implementation

---
+++ ST10026525.PROG3B.POE/Controllers/ReportController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ST10026525.PROG3B.POE.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ReportForm()
+        {
+            // This will look for Views/Report/ReportForm.cshtml by default
+            return View();
+        }
+    }
+}
